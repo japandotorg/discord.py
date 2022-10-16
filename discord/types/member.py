f1efe870cef60c4287590f1075d649206a79ca65
Lemon Rose <yash.kul69@gmail.com>
2022-10-16 20:07:15 +0530

    member types

---
+++ discord/types/member.py
@@ -0,0 +1,41 @@
+from typing import Optional, TypedDict
+from .snowflake import SnowflakeList
+from .user import User
+
+
+class Nickname(TypedDict):
+    nick: str
+
+
+class PartialMember(TypedDict):
+    roles: SnowflakeList
+    joined_at: str
+    deaf: str
+    mute: str
+
+
+class Member(PartialMember, total=False):
+    avatar: str
+    user: User
+    nick: str
+    premium_since: Optional[str]
+    pending: bool
+    permissions: str
+    communication_disabled_until: str
+
+
+class _OptionalMemberWithUser(PartialMember, total=False):
+    avatar: str
+    nick: str
+    premium_since: Optional[str]
+    pending: bool
+    permissions: str
+    comunication_disabled_until: str
+
+
+class MemberWithUser(_OptionalMemberWithUser):
+    user: User
+
+
+class UserWithMember(User, total=False):
+    member: _OptionalMemberWithUser
