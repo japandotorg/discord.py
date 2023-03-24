d50183c55c19a0016861c3b4ede41702cdf18e3d
Lemon Rose <78662983+japandotorg@users.noreply.github.com>
2023-03-24 20:39:37 +0530

    fix

--- discord/http.py
+++ discord/http.py
@@ -504,9 +504,8 @@ class HTTPClient:
         user_agent = 'DiscordBot (https://github.com/Rapptz/discord.py {0}) Python/{1[0]}.{1[1]} aiohttp/{2}'
         self.user_agent = user_agent.format(__version__, sys.version_info, aiohttp.__version__)
         
-        if not INTERNAL_API_BASE.startswith("https://discord.com/api"):
-            self.request = self.request_without_ratelimiter
-    
+    if not INTERNAL_API_BASE.startswith("https://discord.com/api"):
+        self.request = self.request_without_ratelimiter
 
     def recreate(self):
         if self.__session.closed:
