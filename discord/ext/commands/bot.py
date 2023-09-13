99fd1061cae8286d80010012aa9412142395fd8a
Lemon Rose <yash.kul69@gmail.com>
2023-09-13 17:43:08 +0530

    Change default Bot.owner_ids to consider team role

--- discord/ext/commands/bot.py
+++ discord/ext/commands/bot.py
@@ -516,9 +516,13 @@ class BotBase(GroupMixin[None]):
             return user.id in self.owner_ids
         else:
 
-            app = await self.application_info()  # type: ignore
+            app: discord.AppInfo = await self.application_info()  # type: ignore
             if app.team:
-                self.owner_ids = ids = {m.id for m in app.team.members}
+                self.owner_ids = ids = {
+                    m.id
+                    for m in app.team.members
+                    if m.role in (discord.TeamMemberRole.admin, discord.TeamMemberRole.developer)
+                }
                 return user.id in ids
             else:
                 self.owner_id = owner_id = app.owner.id
